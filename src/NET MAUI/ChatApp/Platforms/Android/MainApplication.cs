f33e9e8d07c4b1c3fa8b3d042293cf8a05fb8710
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2022-01-20 20:36:39 +0100

    Complete .NET MAUI sample

--- src/NET MAUI/ChatApp/Platforms/Android/MainApplication.cs
+++ src/NET MAUI/ChatApp/Platforms/Android/MainApplication.cs
@@ -9,6 +9,9 @@ public class MainApplication : MauiApplication
 	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
 		: base(handle, ownership)
 	{
+		Microsoft.Maui.Handlers.EntryHandler.EntryMapper.Add("RemoveBorder", (h, w) => {
+			h.NativeView.Background = null;
+		});
 	}
 
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
