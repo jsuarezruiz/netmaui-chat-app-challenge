3d4ad92c9fa8108cb292d5f0d39561bd3962b6d0
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-05-02 17:11:40 +0200

    Updated sample

---
+++ src/NET MAUI/ChatApp/Converters/ColorToBrushConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace ChatApp.Converters
+{
+    public class ColorToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Color color)
+                return new SolidColorBrush(color);
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
